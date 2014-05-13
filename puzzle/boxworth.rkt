e8b7c890ded7bd4144c6e11886f283e37f83ce6d
jensvarga <jensvarga87@hotmail.com>
2014-05-13 15:40:33 +0200

    Puzzletime

---
+++ puzzle/boxworth.rkt
@@ -0,0 +1,54 @@
+(define riddle
+  (lambda ()
+    (define window (new frame%
+                        [width 500]
+                        [height 500]
+                        [label "BoXWorth"]
+                        ))
+    
+    (define p-callback
+      (lambda (canvas dc)
+        (begin
+          (send dc draw-bitmap a-bitmap 0 0)
+          (send dc draw-rectangle 500 500 0 0)
+          (send dc draw-text 
+                "A box of marbels cost $20. If the marbles are " 90 250))
+        (send dc draw-text 
+                "valued at $19 more than the box" 120 280)
+    (send dc draw-text "What is the box worth?" 170 310)))
+    (define canvas (new canvas%
+                        [parent window]
+                        [paint-callback p-callback] 
+                        ))
+    
+    (define t-callback
+      (lambda (field event)
+        (displayln (string? (send field get-value)))
+        (when (and (eq? (send event get-event-type) 'text-field-enter)
+                   (equal? (send field get-value) "0.50"))
+          (send window show #f))))
+                              
+    (define text (new text-field%
+                      [label "Answer:"]
+                      [parent window]
+                      [callback t-callback]))
+    
+    (send window show #t)))
+
+(define bitmap-canvas%
+  (class canvas%
+    (init-field [bitmap #f])
+    (inherit get-dc)
+    (define/override (on-paint)
+      (send (get-dc) draw-bitmap bitmap 0 0))
+    (super-new)))
+(define a-bitmap (read-bitmap "marbles.png"))
+(define f (new frame% 
+               [label "en bild"] 
+               [width 500] 
+               [height 500]))
+(define the-canvas (new bitmap-canvas% 
+                        [parent f] 
+                        [bitmap a-bitmap]))
+
+(define (bild) (send f show #t))
